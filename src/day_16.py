95ee089e690fd53cfa94ea1e857c825d355d6194
William Bayliff <wabayliff@googlemail.com>
2022-12-22 12:32:08 +0000

    Vastly improved runtime of day 16 solution (now ~ 2 minutes, rather than >8 hours)

--- src/day_16.py
+++ src/day_16.py
@@ -4,7 +4,7 @@ Day 16 challenge.
 
 from math import inf, isinf
 import re
-from typing import Dict, List, Set, Tuple
+from typing import Dict, FrozenSet, List, Set, Tuple
 
 import utils
 
@@ -81,227 +81,117 @@ def _get_distances_table(
 
     return out_dict_ints
 
-def _get_max_pressure(
+def _get_path_to_pressure_map(
+    flow_rates: Dict[str, int],
+    distances_dict: Dict,
+    curr_valve: str,
+    time_left: int,
+    total_so_far: int,
+    valves_activated: Tuple[str, ...],
+    all_valves: Set[str]
+) -> Dict[Tuple[str, ...], int]:
+    """
+    Helper returning dictionary mapping path traversed to total pressure
+    released for each possible path in the given time over the given set of
+    valves.
+
+    When this helper is called, it assumes that:
+    - We are currently at curr_valve, which has (just) been activated.
+    - total_so_far includes the total payoff for all valves activated so far,
+      including curr_valve.
+    - valves_activated is the path traversed so far, including curr_valve.
+    """
+    path_to_pressure_map: Dict[Tuple[str, ...], int] = {
+        valves_activated: total_so_far
+    }
+
+    possible_next_valves = all_valves - set(valves_activated)
+
+    # If we have ran out of time, just return the single path and payoff we know
+    # about, which consists of all valves activated so far.
+    if time_left <= 0:
+        pass
+
+    # If we have already jumped to every node, just return the current path
+    # which contains all valves. The total_so_far already includes the payoff
+    # for every opened valve.
+    elif not possible_next_valves:
+        pass
+
+    # Try every jump to a different valve not yet activated, include all these
+    # paths in the returned set.
+    else:
+        other_paths: Dict[Tuple[str, ...], int] = {}
+
+        assert possible_next_valves
+
+        for valve in possible_next_valves:
+            time_to_jump = distances_dict[(curr_valve, valve)]
+            new_time_left = max(time_left - time_to_jump - 1, 0)
+
+            valve_value = flow_rates[valve]
+            valve_payoff = new_time_left * valve_value
+
+            # Work out the total pressure released if you jump to this valve
+            # and activate it.
+            extra_paths = _get_path_to_pressure_map(
+                flow_rates,
+                distances_dict,
+                curr_valve=valve,
+                time_left=new_time_left,
+                total_so_far=total_so_far + valve_payoff,
+                valves_activated=(valves_activated + (valve,)),
+                all_valves=all_valves,
+            )
+            other_paths.update(extra_paths)
+
+        path_to_pressure_map.update(other_paths)
+
+    return path_to_pressure_map
+
+def _get_path_to_pressure_map_part1(
     flow_rates: Dict[str, int],
     distances_dict: Dict[Tuple[str, str], int],
-) -> int:
+    valves_of_interest: Set[str],
+) -> Dict[Tuple[str, ...], int]:
     """
-    Return the max pressure achievable.
+    Return a map from path taken to total pressure released for part 1.
     """
-    def _get_max_pressure_internal(
-        flow_rates: Dict[str, int],
-        distances_dict: Dict,
-        last_valve: str,
-        time_left: int,
-        total_so_far: int,
-        valves_activated: Set[str],
-        all_valves: Set[str]
-    ) -> int:
-        """
-        Internal helper for doing the recursive step.
-
-        When this helper is called, it assumes that:
-        - We are currently at last_valve, which has (just) been activated.
-        - total_so_far includes the total payoff for all valves activated so
-          far, including last_valve.
-        - valves_activated is the set of valves activated so far, including
-          last_valve.
-        """
-        final_total: int
-
-        # If we have ran out of time, just return the total accumulated so far.
-        if time_left <= 0:
-            final_total = total_so_far
-
-        # If we have already jumped to every node, just return. The total_so_far
-        # already includes the payoff for every opened valve.
-        elif valves_activated == all_valves:
-            final_total = total_so_far
-
-        # Try every jump to a different valve not yet activated, return the best
-        # total pressure.
-        else:
-            total_pressures: List[int] = []
-
-            valves_to_consider = all_valves - valves_activated
-            assert len(valves_to_consider) > 0
-
-            for valve in valves_to_consider:
-                time_to_jump = distances_dict[(last_valve, valve)]
-                new_time_left = max(time_left - time_to_jump - 1, 0)
-
-                valve_value = flow_rates[valve]
-                valve_payoff = new_time_left * valve_value
-
-                # Work out the total pressure released if you jump to this valve
-                # and activate it.
-                total_pressure = _get_max_pressure_internal(
-                    flow_rates,
-                    distances_dict,
-                    last_valve=valve,
-                    time_left=new_time_left,
-                    total_so_far=total_so_far + valve_payoff,
-                    valves_activated=(valves_activated | {valve}),
-                    all_valves=all_valves,
-                )
-                total_pressures.append(total_pressure)
-
-            final_total = max(total_pressures)
-
-        return final_total
-
-    # We are only interested in walking between valves with non-zero flow rate
-    # (and "AA", since we start at that valve).
-    valves_of_interest = {
-        valve for valve in flow_rates if flow_rates[valve] > 0
-    }
-    valves_of_interest.add("AA")
-
     # Call the recursive helper with the correct starting values.
     # Note that we consider "AA" to have been activated- this sets up the
     # recursive helper correctly, since then "AA" doesn't need to be considered
     # again.
-    return _get_max_pressure_internal(
+    return _get_path_to_pressure_map(
         flow_rates,
         distances_dict,
-        last_valve="AA",
+        curr_valve="AA",
         time_left=30,
         total_so_far=0,
-        valves_activated={"AA"},
+        valves_activated=("AA",),
         all_valves=valves_of_interest,
     )
 
-def _get_max_pressure_part2(
+def _get_path_to_pressure_map_part2(
     flow_rates: Dict[str, int],
     distances_dict: Dict[Tuple[str, str], int],
-) -> int:
+    valves_of_interest: Set[str],
+) -> Dict[Tuple[str, ...], int]:
     """
-    Return the max pressure achievable for part 2.
+    Return a map from path taken to total pressure released for part 2.
     """
-    def _get_max_pressure_internal(
-        flow_rates: Dict[str, int],
-        distances_dict: Dict,
-        curr_valve_p1: str,
-        curr_valve_p2: str,
-        overall_time_left: int,
-        time_left_p1: int,
-        time_left_p2: int,
-        total_so_far: int,
-        valves_activated: Set[str],
-        all_valves: Set[str]
-    ) -> int:
-        """
-        Internal helper for doing the recursive step.
-
-        When this helper is called, it assumes that:
-        - time_left_p1 and time_left_p2 give the time until actors 1 and 2 have
-          reached curr_valve_p1 and curr_valve_p2 resp, and activated it.
-        - valves_activated is the set of valves activated so far, or to be
-          activated if either actor has already decided to head to that valve.
-        """
-        final_total: int
-
-        # If both actors are still on the way to the next valve (or activating
-        # it), fast forward time.
-        if time_left_p1 > 0 and time_left_p2 > 0:
-            fast_forward_time = min(time_left_p1, time_left_p2)
-
-            time_left_p1 -= fast_forward_time
-            time_left_p2 -= fast_forward_time
-            overall_time_left -= fast_forward_time
-
-        # If we have ran out of time, just return the total accumulated so far.
-        if overall_time_left <= 0:
-            final_total = total_so_far
-
-        # If we have already jumped to every node, just return. The total_so_far
-        # already includes the payoff for every opened valve.
-        elif valves_activated == all_valves:
-            final_total = total_so_far
-
-        # Figure out which actor is ready to make a new hop decision. Try every
-        # jump to a different valve not yet activated, return the best total
-        # pressure.
-        else:
-            if time_left_p1 == 0:
-                curr_valve = curr_valve_p1
-                moving_actor = 1
-            elif time_left_p2 == 0:
-                curr_valve = curr_valve_p2
-                moving_actor = 2
-            else:
-                assert False
-
-            total_pressures: List[int] = []
-
-            valves_to_consider = all_valves - valves_activated
-            assert len(valves_to_consider) > 0
-
-            for valve in valves_to_consider:
-                time_to_jump = distances_dict[(curr_valve, valve)]
-                new_time_left = time_to_jump + 1
-
-                valve_value = flow_rates[valve]
-                valve_lifetime = max(overall_time_left - new_time_left, 0)
-                valve_payoff = valve_lifetime * valve_value
-
-                if moving_actor == 1:
-                    new_curr_valve_p1 = valve
-                    new_curr_valve_p2 = curr_valve_p2
-                    new_time_left_p1  = new_time_left
-                    new_time_left_p2  = time_left_p2
-                elif moving_actor == 2:
-                    new_curr_valve_p1 = curr_valve_p1
-                    new_curr_valve_p2 = valve
-                    new_time_left_p1  = time_left_p1
-                    new_time_left_p2  = new_time_left
-                else:
-                    assert False
-
-                # Work out the total pressure released if you jump to this valve
-                # and activate it.
-                total_pressure = _get_max_pressure_internal(
-                    flow_rates,
-                    distances_dict,
-                    curr_valve_p1=new_curr_valve_p1,
-                    curr_valve_p2=new_curr_valve_p2,
-                    overall_time_left=overall_time_left,
-                    time_left_p1=new_time_left_p1,
-                    time_left_p2=new_time_left_p2,
-                    total_so_far=total_so_far + valve_payoff,
-                    valves_activated=(valves_activated | {valve}),
-                    all_valves=all_valves,
-                )
-                total_pressures.append(total_pressure)
-
-            final_total = max(total_pressures)
-
-        return final_total
-
-    # We are only interested in walking between valves with non-zero flow rate
-    # (and "AA", since we start at that valve).
-    valves_of_interest = {
-        valve for valve in flow_rates if flow_rates[valve] > 0
-    }
-    valves_of_interest.add("AA")
-
     # Call the recursive helper with the correct starting values.
     # Note that we consider "AA" to have been activated- this sets up the
     # recursive helper correctly, since then "AA" doesn't need to be considered
     # again.
-    return _get_max_pressure_internal(
+    return _get_path_to_pressure_map(
         flow_rates,
         distances_dict,
-        curr_valve_p1="AA",
-        curr_valve_p2="AA",
-        overall_time_left=26,
-        time_left_p1=0,
-        time_left_p2=0,
+        curr_valve="AA",
+        time_left=26,
         total_so_far=0,
-        valves_activated={"AA"},
+        valves_activated=("AA",),
         all_valves=valves_of_interest,
     )
-
 
 #
 # Solution starts here
@@ -309,10 +199,44 @@ def _get_max_pressure_part2(
 flow_rates, adj_dict = _parse_data_file(DATA_FILE)
 distances_dict = _get_distances_table(adj_dict)
 
+# We are only interested in walking between valves with non-zero flow rate
+# (and "AA", since we start at that valve).
+valves_of_interest = {
+    valve for valve in flow_rates if flow_rates[valve] > 0
+}
+valves_of_interest.add("AA")
+
 # Part 1
-max_pressure = _get_max_pressure(flow_rates, distances_dict)
+paths = _get_path_to_pressure_map_part1(
+    flow_rates, distances_dict, valves_of_interest
+)
+
+max_pressure = sorted(paths.values(), reverse=True)[0]
 print(f"Part 1, max pressure: {max_pressure}")
 
-# Part 2
-max_pressure_2 = _get_max_pressure_part2(flow_rates, distances_dict)
-print(f"Part 2, max pressure: {max_pressure_2}")
+# Part 2 - The strategy here is to re-use the work from part 1 to get a mapping
+#          from path to payoff, for each path within the set valves with
+#          non-zero flow. We then look for the pair of disjoint paths with the
+#          biggest total payoff: these are the paths that the elephant and I
+#          should take.
+path_map = _get_path_to_pressure_map_part2(
+    flow_rates, distances_dict, valves_of_interest
+)
+
+# Consolidate paths (for a given path, all we care about is which valves were
+# covered, not the order in which they were covered).
+consolidated_paths: Dict[FrozenSet[str], int] = {}
+for path, val in path_map.items():
+    path_set = frozenset(path)
+
+    old_val = consolidated_paths.get(path_set, 0)
+    consolidated_paths[path_set] = max(old_val, val)
+
+# Look for disjoint paths, record their payoff.
+total_pressures: List[int] = []
+for path1, val1 in consolidated_paths.items():
+    for path2, val2 in consolidated_paths.items():
+        if path1 & path2 == {"AA"}:
+            total_pressures.append(val1 + val2)
+
+print(f"Part 2, max pressure: {max(total_pressures)}")
